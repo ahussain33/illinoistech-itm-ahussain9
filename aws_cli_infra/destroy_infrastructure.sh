50991515825c86b06e75debec7f3c427dc625228
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2025-11-08 18:45:28 -0600

    Update destroy_infrastructure.sh

--- aws_cli_infra/destroy_infrastructure.sh
+++ aws_cli_infra/destroy_infrastructure.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+source config.txt
+source cloudwatch_utils.sh
+log_to_cw "Destroying infrastructure..."
+
+while read INSTANCE_ID; do
+aws ec2 terminate-instances --instance-ids "$INSTANCE_ID" --region "$REGION"
+aws ec2 wait instance-terminated --instance-ids "$INSTANCE_ID" --region "$REGION"
+done < instance_id.txt
+
+aws ec2 delete-key-pair --key-name "$KEY_NAME" --region "$REGION"
+
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=$SECURITY_GROUP_NAME" \
+--query "SecurityGroups[0].GroupId" --output text --region "$REGION")
+aws ec2 delete-security-group --group-id "$SG_ID" --region "$REGION"
+
+VPC_ID=$(cat vpc_id.txt)
+aws ec2 delete-vpc --vpc-id "$VPC_ID" --region "$REGION"
+
+rm -f instance_id.txt instance_ip.txt vpc_id.txt
+log_to_cw "Infrastructure destroyed successfully"
+send_cw_metric 1
