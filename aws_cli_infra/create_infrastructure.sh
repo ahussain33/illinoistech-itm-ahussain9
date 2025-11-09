bf2c9cb74c55fb250573e91ca7fc62ee62391606
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2025-11-08 18:02:45 -0600

    Update create_infrastructure.sh

--- aws_cli_infra/create_infrastructure.sh
+++ aws_cli_infra/create_infrastructure.sh
@@ -48,3 +48,25 @@ AMI_ID=$(aws ec2 describe-images --owners "$UBUNTU_OWNER" \
 --filters "Name=name,Values=$UBUNTU_FILTER" \
 --region "$REGION" \
 --query "Images | sort_by(@, &CreationDate) | [-1].ImageId" --output text)
+
+INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --count 1 \
+  --instance-type "$INSTANCE_TYPE" --key-name "$KEY_NAME" \
+  --security-group-ids "$SG_ID" --subnet-id "$SUBNET1" --associate-public-ip-address \
+  --region "$REGION" --query "Instances[0].InstanceId" --output text)
+
+aws ec2 wait instance-running --instance-ids "$INSTANCE_ID" --region "$REGION"
+
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
+  --query "Reservations[0].Instances[0].PublicIpAddress" \
+  --output text --region "$REGION")
+
+echo "$INSTANCE_ID" > instance_id.txt
+echo "$PUBLIC_IP" > instance_ip.txt
+log_to_cw "EC2 instance created: $INSTANCE_ID ($PUBLIC_IP)"
+send_cw_metric 1
+
+
+
+
+
+
